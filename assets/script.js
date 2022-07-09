d1a6e9ff7b46e54b62ceeae1b57353eb40b61854
Appoline Cogan <appolinecogan@gmail.com>
2022-07-09 19:14:57 +0700

    updated get item function from local storage

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
   var savedContent9 = localStorage.getItem("content9");
-  $("#content9").text = savedContent9;
+  $("#content9").val(savedContent9);
+
+  var savedContent10 = localStorage.getItem("content10");
+  $("#content10").val(savedContent10);
+
+  var savedContent11 = localStorage.getItem("content11");
+  $("#content11").val(savedContent11);
+
+  var savedContent12 = localStorage.getItem("content12");
+  $("#content12").val(savedContent12);
+
+  var savedContent13 = localStorage.getItem("content13");
+  $("#content13").val(savedContent13);
+
+  var savedContent14 = localStorage.getItem("content14");
+  $("#content14").val(savedContent14);
+
+  var savedContent15 = localStorage.getItem("content15");
+  $("#content15").val(savedContent15);
+
+  var savedContent16 = localStorage.getItem("content16");
+  $("#content16").val(savedContent16);
+
+  var savedContent17 = localStorage.getItem("content17");
+  $("#content17").val(savedContent17);
 });
 // I need to display the date using moment.js
 
@@ -25,8 +49,47 @@ content.each(function () {
   }
 });
 
-var contentHour9 = $("#content9").value;
-
 $("#saveBtn9").on("click", function () {
-  localStorage.setItem("content9", contentHour9);
+  var contentHour9 = $("#content9").val();
+  local;
+  Storage.setItem("content9", contentHour9);
+});
+$("#saveBtn10").on("click", function () {
+  var contentHour10 = $("#content10").val();
+  localStorage.setItem("content10", contentHour10);
+});
+
+$("#saveBtn11").on("click", function () {
+  var contentHour11 = $("#content11").val();
+  localStorage.setItem("content11", contentHour11);
+});
+
+$("#saveBtn12").on("click", function () {
+  var contentHour12 = $("#content12").val();
+  localStorage.setItem("content12", contentHour12);
+});
+
+$("#saveBtn13").on("click", function () {
+  var contentHour13 = $("#content13").val();
+  localStorage.setItem("content13", contentHour13);
+});
+
+$("#saveBtn14").on("click", function () {
+  var contentHour14 = $("#content14").val();
+  localStorage.setItem("content14", contentHour14);
+});
+
+$("#saveBtn15").on("click", function () {
+  var contentHour15 = $("#content15").val();
+  localStorage.setItem("content15", contentHour15);
+});
+
+$("#saveBtn16").on("click", function () {
+  var contentHour16 = $("#content16").val();
+  localStorage.setItem("content16", contentHour16);
+});
+
+$("#saveBtn17").on("click", function () {
+  var contentHour17 = $("#content17").val();
+  localStorage.setItem("content17", contentHour17);
 });
