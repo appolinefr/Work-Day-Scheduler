114a853ee1f9cdfd9766ed950571666cc48fbd96
Appoline Cogan <appolinecogan@gmail.com>
2022-07-09 23:59:50 +0700

    fixed timeblock function copmaring timblock with current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
   var savedContent9 = localStorage.getItem("content9");
-  $("#content9").val(savedContent9);
+  $("#9").val(savedContent9);
 
   var savedContent10 = localStorage.getItem("content10");
-  $("#content10").val(savedContent10);
+  $("#10").val(savedContent10);
 
   var savedContent11 = localStorage.getItem("content11");
-  $("#content11").val(savedContent11);
+  $("#11").val(savedContent11);
 
   var savedContent12 = localStorage.getItem("content12");
-  $("#content12").val(savedContent12);
+  $("#12").val(savedContent12);
 
   var savedContent13 = localStorage.getItem("content13");
-  $("#content13").val(savedContent13);
+  $("13").val(savedContent13);
 
   var savedContent14 = localStorage.getItem("content14");
-  $("#content14").val(savedContent14);
+  $("#14").val(savedContent14);
 
   var savedContent15 = localStorage.getItem("content15");
-  $("#content15").val(savedContent15);
+  $("#15").val(savedContent15);
 
   var savedContent16 = localStorage.getItem("content16");
-  $("#content16").val(savedContent16);
+  $("#16").val(savedContent16);
 
   var savedContent17 = localStorage.getItem("content17");
-  $("#content17").val(savedContent17);
+  $("#17").val(savedContent17);
 });
 // I need to display the date using moment.js
 
@@ -42,67 +42,73 @@ content.each(function () {
   //this will add class to each timeblock depending on whether they are in future, past or present
   if (timeBlock > currentHour) {
     $(this).addClass("future");
+    $(this).removeClass("past");
+    $(this).removeClass("present");
   } else if (timeBlock === currentHour) {
     $(this).addClass("present");
+    $(this).removeClass("past");
+    $(this).removeClass("future");
   } else {
     $(this).addClass("past");
+    $(this).removeClass("future");
+    $(this).removeClass("present");
   }
 });
 
 $("#saveBtn9").on("click", function () {
-  var contentHour9 = $("#content9").val();
+  var contentHour9 = $("#9").val();
   localStorage.setItem("content9", contentHour9);
 });
 
 $("#saveBtn10").on("click", function () {
-  var contentHour10 = $("#content10").val();
+  var contentHour10 = $("#10").val();
   localStorage.setItem("content10", contentHour10);
 });
 
 $("#saveBtn11").on("click", function () {
-  var contentHour11 = $("#content11").val();
+  var contentHour11 = $("#11").val();
   localStorage.setItem("content11", contentHour11);
 });
 
 $("#saveBtn12").on("click", function () {
-  var contentHour12 = $("#content12").val();
+  var contentHour12 = $("#12").val();
   localStorage.setItem("content12", contentHour12);
 });
 
 $("#saveBtn13").on("click", function () {
-  var contentHour13 = $("#content13").val();
+  var contentHour13 = $("#13").val();
   localStorage.setItem("content13", contentHour13);
 });
 
 $("#saveBtn14").on("click", function () {
-  var contentHour14 = $("#content14").val();
+  var contentHour14 = $("#14").val();
   localStorage.setItem("content14", contentHour14);
 });
 
 $("#saveBtn15").on("click", function () {
-  var contentHour15 = $("#content15").val();
+  var contentHour15 = $("#15").val();
   localStorage.setItem("content15", contentHour15);
 });
 
 $("#saveBtn16").on("click", function () {
-  var contentHour16 = $("#content16").val();
+  var contentHour16 = $("#16").val();
   localStorage.setItem("content16", contentHour16);
 });
 
 $("#saveBtn17").on("click", function () {
-  var contentHour17 = $("#content17").val();
+  var contentHour17 = $("#17").val();
   localStorage.setItem("content17", contentHour17);
 });
 
 $("#clearBtn").on("click", function () {
   localStorage.clear();
-  $("#content9").val("");
-  $("#content10").val("");
-  $("#content11").val("");
-  $("#content12").val("");
-  $("#content13").val("");
-  $("#content14").val("");
-  $("#content15").val("");
-  $("#content16").val("");
-  $("#content17").val("");
+  $("#9").val("");
+  $("#10").val("");
+  $("#11").val("");
+  $("#12").val("");
+  $("#13").val("");
+  $("#14").val("");
+  $("#15").val("");
+  $("#16").val("");
+  $("#17").val("");
 });
