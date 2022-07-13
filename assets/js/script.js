a9fb4dd4d5d7574f851df0ed5b35512bd7e9cc0d
Appoline Cogan <appolinecogan@gmail.com>
2022-07-13 17:43:12 +0700

    simplified code for local storage with for loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,9 @@
 //getting all elements from storage and displaying them on page
 $(document).ready(function () {
-  var savedContent9 = localStorage.getItem("content9");
-  $("#9").val(savedContent9);
-
-  var savedContent10 = localStorage.getItem("content10");
-  $("#10").val(savedContent10);
-
-  var savedContent11 = localStorage.getItem("content11");
-  $("#11").val(savedContent11);
-
-  var savedContent12 = localStorage.getItem("content12");
-  $("#12").val(savedContent12);
-
-  var savedContent13 = localStorage.getItem("content13");
-  $("13").val(savedContent13);
-
-  var savedContent14 = localStorage.getItem("content14");
-  $("#14").val(savedContent14);
-
-  var savedContent15 = localStorage.getItem("content15");
-  $("#15").val(savedContent15);
-
-  var savedContent16 = localStorage.getItem("content16");
-  $("#16").val(savedContent16);
-
-  var savedContent17 = localStorage.getItem("content17");
-  $("#17").val(savedContent17);
+  for (let index = 9; index < 18; index++) {
+    var savedContent = localStorage.getItem("content" + index);
+    $("#" + index).val(savedContent);
+  }
 });
 
 //displaying date
@@ -57,62 +35,17 @@ content.each(function () {
   }
 });
 
-//event listener on each button that will save content for each time block
-$("#saveBtn9").on("click", function () {
-  var contentHour9 = $("#9").val();
-  localStorage.setItem("content9", contentHour9);
-});
+$(".custom-button").on("click", function () {
+  var contentValue = $(this).siblings(".content").val();
+  var contentHour = $(this).siblings(".content").attr("id");
 
-$("#saveBtn10").on("click", function () {
-  var contentHour10 = $("#10").val();
-  localStorage.setItem("content10", contentHour10);
-});
-
-$("#saveBtn11").on("click", function () {
-  var contentHour11 = $("#11").val();
-  localStorage.setItem("content11", contentHour11);
-});
-
-$("#saveBtn12").on("click", function () {
-  var contentHour12 = $("#12").val();
-  localStorage.setItem("content12", contentHour12);
-});
-
-$("#saveBtn13").on("click", function () {
-  var contentHour13 = $("#13").val();
-  localStorage.setItem("content13", contentHour13);
-});
-
-$("#saveBtn14").on("click", function () {
-  var contentHour14 = $("#14").val();
-  localStorage.setItem("content14", contentHour14);
-});
-
-$("#saveBtn15").on("click", function () {
-  var contentHour15 = $("#15").val();
-  localStorage.setItem("content15", contentHour15);
-});
-
-$("#saveBtn16").on("click", function () {
-  var contentHour16 = $("#16").val();
-  localStorage.setItem("content16", contentHour16);
-});
-
-$("#saveBtn17").on("click", function () {
-  var contentHour17 = $("#17").val();
-  localStorage.setItem("content17", contentHour17);
+  localStorage.setItem("content" + contentHour, contentValue);
 });
 
 //the clear button will clear the local storage
 $("#clearBtn").on("click", function () {
+  for (let index = 9; index < 18; index++) {
+    $("#" + index).val("");
+  }
   localStorage.clear();
-  $("#9").val("");
-  $("#10").val("");
-  $("#11").val("");
-  $("#12").val("");
-  $("#13").val("");
-  $("#14").val("");
-  $("#15").val("");
-  $("#16").val("");
-  $("#17").val("");
 });
